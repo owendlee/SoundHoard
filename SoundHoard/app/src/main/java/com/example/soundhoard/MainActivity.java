9d89bf2735f055951fe152477ea57292b669aa70
edwardchang7 <echang@unb.ca>
2020-02-16 02:31:48 -0400

    Removing even more unused code

--- SoundHoard/app/src/main/java/com/example/soundhoard/MainActivity.java
+++ SoundHoard/app/src/main/java/com/example/soundhoard/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 
