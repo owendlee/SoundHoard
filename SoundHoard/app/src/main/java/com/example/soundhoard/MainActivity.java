38c756f84a35f9e602b09b09e076080fefa26456
edwardchang7 <echang@unb.ca>
2020-02-15 23:10:09 -0400

    Adding two java class for starting activity from the main screen. Also updating strings.xml file

--- SoundHoard/app/src/main/java/com/example/soundhoard/MainActivity.java
+++ SoundHoard/app/src/main/java/com/example/soundhoard/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.soundhoard;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button soundboardsButton;
+    private Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        soundboardsButton = findViewById(R.id.soundboardsButton);
+        soundboardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Soundboards.class);
+                startActivity(intent);
+            }
+        });
+
+        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ SoundHoard/app/src/main/java/com/example/soundhoard/Settings.java
@@ -0,0 +1,4 @@
+package com.example.soundhoard;
+
+public class Settings {
+}
