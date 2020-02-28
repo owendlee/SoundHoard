31462cc785be9dc09fdc608237cf660c459394e1
edwardchang7 <echang@unb.ca>
2020-02-28 14:49:54 -0400

    Adding ability to edit and delete soundboard

--- SoundHoard/app/src/main/java/com/example/soundhoard/SoundboardActivity.java
+++ SoundHoard/app/src/main/java/com/example/soundhoard/SoundboardActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class SoundboardActivity extends AppCompatActivity {
@@ -16,6 +18,13 @@ public class SoundboardActivity extends AppCompatActivity {
         String soundboardName = intent.getStringExtra("soundboardName");
         getSupportActionBar().setTitle(soundboardName);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_soundboard_menu, menu);
+        return true;
     }
 
     @Override
